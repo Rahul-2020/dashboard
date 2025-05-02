dc981b1f5fa69aef31c1f04e4af2f5b0db23c885
Rahul-2020 <51008194+Rahul-2020@users.noreply.github.com>
2025-05-02 12:00:25 -0700

    fix ts error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -118,6 +118,7 @@ export async function authenticate(
     await signIn('credentials', formData);
   } catch (error) {
     if (error instanceof AuthError) {
+      // @ts-ignore
       switch (error.type) {
         case 'CredentialsSignin':
           return 'Invalid credentials.';
